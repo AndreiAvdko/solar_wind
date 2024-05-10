f1b2104eab9dc0b713c6e077e25c86416243d2f0
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 13:12:12 +0300

    debug

--- solar_wind/sw_utils/sw_utils.py
+++ solar_wind/sw_utils/sw_utils.py
@@ -102,15 +102,13 @@ def create_folder_if_not_exist(folder_path):
 
 def get_conf_path(path=None, only_root_path=False) -> str:
     # TODO Удалить отладку
-
-    print(f"Это путь: {os.getcwd()}")
     print(f"Это абсолютный путь до скрипта: {os.path.abspath(__file__)}")
 
     if path is None:
         root_project_dir = os.path.abspath(__file__) # TODO os.getcwd()
         while not os.path.exists(os.path.join(root_project_dir, 'project_conf.json')):
             # TODO Удалить отладку
-            print(f"Проверяем этот путь {root_project_dir}project_conf.json")
+            print(f"Проверяем этот путь {os.path.join(root_project_dir, 'project_conf.json')}")
             root_project_dir = os.path.dirname(root_project_dir)
         if only_root_path:
             return root_project_dir
