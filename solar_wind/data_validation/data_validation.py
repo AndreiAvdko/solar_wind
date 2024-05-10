90343aeffa77d2133747bb5fa24daab2e86f6438
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 20:11:33 +0300

    add saving clean data

--- solar_wind/data_validation/data_validation.py
+++ solar_wind/data_validation/data_validation.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from solar_wind.custom_exception import MissingColumnsError, IncorrectTimestamps
@@ -34,6 +36,7 @@ class DataValidator:
         # plt.plot(clean_df[["v_plasma"]])
         # plt.title(f"Значения скорости солнечного ветра")
         # plt.show()
+        clean_df.to_csv(f'{swu.get_clean_data_path()}clean_data_for_predict_from_{datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.csv', index=False)
         return clean_df
 
     def __check_and_process_timestamp(self):
