cb92f8947df8ee50707ef59aa0c6aece00f21d78
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 14:48:48 +0300

    debug

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     name='solar_wind',
     version='0.1',
     python_requires='>=3.8.0, <3.11.0',
-    packages=find_packages(where='.'),
-    package_data={'': ['*.json', '*.dat', '*.csv']},  # Включить project_conf.json
+    packages=find_packages(),
+    package_data={
+        '': ['*.json', '*.dat', '*.csv']
+    },
     install_requires=[
         'etna',
         'etna[auto]',
