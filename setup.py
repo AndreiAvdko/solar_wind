b462a6f0b2a083fc0727b3fb2e55d0a5dca04846
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 17:36:30 +0300

    debug

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1',
     python_requires='>=3.8.0, <3.11.0',
     packages=find_packages(),
-    # package_dir={"": "solar_wind "}, возникает ошибка
+    # package_dir={"": "solar_wind"}, возникает ошибка
     include_package_data=True,
     package_data={
         '': ['*.json', '*.dat', '*.csv'],
-        'test_data_validation.test_data.correct_timestamps': ["*.csv"],
+        'test_data_validation.test_data.correct_timestamps': ['*.csv'],
     },
     install_requires=[
         'etna',
