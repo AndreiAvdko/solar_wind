187bfe884bef299fba31c6a3773db1e5754c7dfb
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 16:15:37 +0300

    debug

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ setup(
     version='0.1',
     python_requires='>=3.8.0, <3.11.0',
     packages=find_packages(),
+    package_dir={'': 'solar_wind'},
     package_data={
-        '': ['*.*']
+        # "mypkg": ["*.txt"],
+        # "mypkg.data": ["*.rst"],
+        'solar_wind': ['*.json'],
+        'test_data_validation.test_data': ['*.csv', '*.dat'],
+        'solar_wind.model': ['*.pkl'],
     },
     install_requires=[
         'etna',
