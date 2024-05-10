3c19ce51e3362f85f2d28a72e3da010472e57e19
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 17:03:52 +0300

    debug

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     python_requires='>=3.8.0, <3.11.0',
     packages=find_packages(),
     package_data={
-        '': ['*.json', '*.dat', '*.csv']
+        '': ['*.json', '*.dat', '*.csv'],
+        'test_data_validation': ['test_data/*.json', 'test_data/*.dat', 'test_data/*.csv'],
     },
     # package_dir={"": "solar_wind "}, возникает ошибка
 
