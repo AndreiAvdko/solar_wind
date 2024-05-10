7a6dd3af2acf7b04c9383e888555813ca60cfca2
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 15:07:59 +0300

    debug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     python_requires='>=3.8.0, <3.11.0',
     packages=find_packages(),
     package_data={
-        '': ['*.json', '*.dat', '*.csv']
+        '': ['*.*']
     },
     install_requires=[
         'etna',
