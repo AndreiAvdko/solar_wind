d91f7c0a6c2e67fa64be5a6b594eb77ca71bbf61
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-05-10 17:09:27 +0300

    debug

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ setup(
     version='0.1',
     python_requires='>=3.8.0, <3.11.0',
     packages=find_packages(),
-    package_data={
-        '': ['*.json', '*.dat', '*.csv'],
-        'test_data_validation': ['test_data/*.json', 'test_data/*.dat', 'test_data/*.csv'],
-    },
     # package_dir={"": "solar_wind "}, возникает ошибка
-
     include_package_data=True,
+    package_data={
+        '': ['*.json', '*.dat', '*.csv', 'test_data/*.json', 'test_data/*.dat', 'test_data/*.csv'],
+    },
     install_requires=[
         'etna',
         'etna[auto]',
